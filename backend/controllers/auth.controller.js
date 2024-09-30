e513e7bf286ff86796f0262a5d9604f851328a32
Javier Flores <jffc.dev@gmail.com>
2024-09-29 20:20:22 -0500

    feat: controllers and other items added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import { User } from "../models/user.model.js"
+import bcryptjs from 'bcryptjs'
+import { generateVerificationToken } from "../utils/generateVerificationToken.js"
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"
+import { sendVerificationEmail } from "../mailtrap/emails.js"
+
+export const signup = async (req, res)=> {
+    const {email, password, name} = req.body
+    
+    try {
+        if(!email || !password || !name){
+            throw new Error('All fields are required')
+        }
+
+        const userAlreadyExists = await User.findOne({email})
+        if(userAlreadyExists){
+            return res.status(400).json({success: false, message: 'user already exists'})
+        }
+        const hashedPassword = await bcryptjs.hash(password, 10)
+        const verificationToken = generateVerificationToken()
+        const user = new User({email, password: hashedPassword, name, verificationToken, verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000})
+        
+        await user.save()
+        generateTokenAndSetCookie(res, user._id)
+        sendVerificationEmail(user.email, verificationToken)
+
+        return res.status(400).json({success: true, message: 'user created successfully', user: {
+            ...user._doc, password: undefined
+        }})
+    } catch (error) {
+        return res.status(400).json({success: false, message: error.message})
+    }
+}
+
+export const login = async (req, res)=> {
+    res.send("login route")
+}
+
+export const logout = async (req, res)=> {
+    res.send("logout route")
+}
---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './db/connectDB.js'
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.get("/", (req, res)=> {
+    res.send("Hello world1!")
+})
+
+app.use("/api/auth", authRoutes)
+
+app.listen(3001, ()=>{
+    connectDB()
+    console.log(`server is running on port: ${port}`);
+    
+})
