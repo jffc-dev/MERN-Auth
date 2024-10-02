0ca703aefd1c7d0a8c420ff2e2818b743898d787
Javier Flores <jffc.dev@gmail.com>
2024-10-02 15:53:19 -0500

    feat: useAuthStore and cors config added

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 import { connectDB } from './db/connectDB.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors({origin: 'http://localhost:5173', credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
@@ -17,7 +19,7 @@ app.get("/", (req, res)=> {
 
 app.use("/api/auth", authRoutes)
 
-app.listen(3001, ()=>{
+app.listen(port, ()=>{
     connectDB()
     console.log(`server is running on port: ${port}`);
     
