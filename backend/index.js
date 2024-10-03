53924bb719684ee8cdd72f0ac9c5ce89b399ac23
Javier Flores <jffc.dev@gmail.com>
2024-10-03 10:54:58 -0500

    feat: changes for production

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,30 @@ import { connectDB } from './db/connectDB.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(cors({origin: 'http://localhost:5173', credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
-app.get("/", (req, res)=> {
-    res.send("Hello world1!")
-})
-
 app.use("/api/auth", authRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+    console.log(2)
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+    app.get('*', (req, res)=>{
+        console.log(1)
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(port, ()=>{
     connectDB()
     console.log(`server is running on port: ${port}`);
-    
+    console.log(`server is running on mode: '${process.env.NODE_ENV}'`);
 })
